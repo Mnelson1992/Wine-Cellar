32936d7422504517488435a5d7e728aee772a719
Mnelson1992 <nelson.matthew2015@gmail.com>
2017-12-04 20:34:17 -0500

    Created the form to make a new bottle

--- app/controllers/bottles_controller.rb
+++ app/controllers/bottles_controller.rb
@@ -2,10 +2,28 @@ class BottlesController < ApplicationController
 
   get '/bottles' do
     if logged_in?
-      @bottles = Bottle.all
+      @bottle = Bottle.all
       erb :'bottles/show_bottle'
     else
       redirect to '/login'
+    end
+  end
+
+  get '/bottles/new' do
+    if logged_in?
+      erb :'/bottles/create_bottle'
+    else
+      redirect to '/login'
+    end
+  end
+
+  post '/bottles' do
+    if params[:name] != ""
+      @bottle = Bottle.new(name: params[:name], type: params[:tpye], year: params[:year], location: [:location])
+      @bottle.user_id=session[:user_id]
+      @bottle.save
+    else
+      redirect to '/bottles/new'
     end
   end
 
