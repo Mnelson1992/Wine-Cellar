8d544f0bbfac137b6b9bb433852c323e3068cdca
Mnelson1992 <nelson.matthew2015@gmail.com>
2017-12-04 19:38:35 -0500

    Added login and logout routes to the user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,10 +6,39 @@ class UsersController < ApplicationController
     else
       redirect to '/bottles'
     end
-  end 
+  end
 
+  post '/signup' do
+    if params[:email] == "" || params[:username] == "" || params[:password] == ""
+      redirect '/signup'
+    else
+      @user = User.create(email: params[:email], username: params[:username], password: params[:password])
+      session[:user_id] = user.id
+      redirect '/bottles'
+    end
+  end
 
+  get '/login' do
+    if !logged_in?
+      erb :'users/login'
+    else
+      redirect to '/bottles'
+    end
+  end
 
+  post '/login' do
+    user = User.find_by(:username => params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect '/bottles'
+    else
+      redirect '/signup'
+    end
+  end
 
+  get '/logout' do
+    session.clear
+    redirect to '/login'
+  end
 
 end
