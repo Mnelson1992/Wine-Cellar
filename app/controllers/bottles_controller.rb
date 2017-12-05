703fb292fd1d2105b77d6dbd286bd7b6d5bf82e0
Mnelson1992 <nelson.matthew2015@gmail.com>
2017-12-04 19:48:31 -0500

    Defined my get route for bottles

--- app/controllers/bottles_controller.rb
+++ app/controllers/bottles_controller.rb
@@ -1,5 +1,14 @@
 class BottlesController < ApplicationController
 
+  get '/bottles' do
+    if logged_in?
+      @bottles = Bottle.all
+      erb :'bottles/show_bottle'
+    else
+      redirect to '/login'
+    end
+  end
+
 
 
 
